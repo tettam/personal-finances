21ead30333b85ec081733df229538e6f009c21c0
Marco Tettamanti <marcotettamanti@outlook.com.br>
2023-07-12 04:55:58 -0300

    Created category and item type

---
+++ src/app/data/items.ts
@@ -0,0 +1,8 @@
+import { Item } from "../types/Item";
+
+export const items: Item[] = [
+  {date: new Date(2021,9,15), category:'food', title:'McDonals', value: 32.32},
+  {date: new Date(2021,9,15), category:'food', title:'Burger King', value: 28},
+  {date: new Date(2021,9,15), category:'food', title:'Aluguel Apt', value: 2300},
+  {date: new Date(2021,9,15), category:'food', title:'Salário', value: 4500}
+]
